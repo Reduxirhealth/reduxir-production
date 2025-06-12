b539d6e56325b23255477a87f64d438cc8eabb5e
Reduxirhealth <elvinneddall@gmail.com>
2025-06-12 17:05:59 -0400

    Delete reduxir_production_20250612_200743 directory

--- reduxir_production_20250612_200743/src/main.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './styles/index.css';
-import App from './routes/App';
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
--- reduxir_production_20250612_200743/src/routes/App.jsx
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-export default function App() {
-  return <div className='text-center p-10'>Reduxir Health and Wellness App</div>;
-}
